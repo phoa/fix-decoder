15193ba60d8bdd61ab30951971680709dbec004b
paul nikolas phoa <nikolas@phoa.me>
2016-10-23 23:01:19 +0800

    add github ribbon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import GithubRibbon from './components/GithubRibbon';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <GithubRibbon
+          githubLink="https://github.com/phoa/fix-decoder"
+        />
         <div className="App-header">
           <h2>FIX Decoder</h2>
         </div>
