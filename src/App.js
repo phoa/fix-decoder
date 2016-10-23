137f43402bf264e4b3d30811c74c3fd398f380fc
paul nikolas phoa <nikolas@phoa.me>
2016-10-23 16:11:27 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>FIX Decoder</h2>
+        </div>
+        <p className="App-intro">
+          Coming soon...
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import '../node_modules/sanitize.css/sanitize.css';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
